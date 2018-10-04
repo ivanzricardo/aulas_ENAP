f0e4cdcc2e3308897c5d718ddbbc8aebca3d5ae0
Ivan Zimmermann <ivanzricardo@gmail.com>
2018-10-04 11:52:48 -0300

    Inseridas as resoluções em sala

--- CADS2018/Exercícios/Exerc_05.R
+++ CADS2018/Exercícios/Exerc_05.R
@@ -12,7 +12,7 @@ gc()
 
 # Carregue o arquivo `decisoes.rds` em um objeto chamado `decisoes`. ----
 
-decisoes <- read_rds("C:/Users/Aluno/Desktop/git/aulas_ENAP/CADS2018/Exercícios/dados/decisoes.rds")
+decisoes <- read_rds("C:/Users/aluno.ENAP/Desktop/decisoes.rds")
 
 # Qual quantidade mensal de decisões por juiz?
 
@@ -20,11 +20,29 @@ juiz_mes <- decisoes %>%
   mutate(mes = month(dmy(data_decisao))) %>%
   filter(!is.na(mes)) %>%
   group_by(juiz,mes) %>%
-  summarise(n=n()) %>%
-  spread(mes,n,fill = 0)
+  summarise(qntde_processos=n()) %>%
+  spread(mes,qntde_processos,fill = 0)
 
+# Qual quantidade de processos porassunto?
+
+decisoes %>% 
+  select(n_processo, classe_assunto) %>% 
+  separate(classe_assunto, c('classe', 'assunto'), sep = ' / ', 
+           extra = 'merge', fill = 'right') %>% 
+  count(assunto, sort = TRUE)
+
+#Abri um projet aninhado (nested)
+processos_nested <- readRDS("~/Exercicios_ENAP/CADS2018/Exercícios/dados/processos_nested.rds")
+
+#Desaninhar o processos por partes
+
+processos_partes <- processos_nested %>%
+  select(n_processo, partes) %>%
+  unnest(partes)
 
 # Crie um objeto contendo informações sobre os tamanhos das bancadas dos ----
+
+
 # partidos (arquivo `bancadas.rds`), suas respectivas coligações 
 # eleitorais para 2018 (arquivo `coligacoes.xlsx`) e o 
 # grau de concordância com a agenda do Gov 
