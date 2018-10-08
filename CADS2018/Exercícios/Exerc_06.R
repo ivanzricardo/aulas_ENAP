073d92faf813d89e37c49ea296baad6e26cbe084
Frederico Bertholini <fredericobertholini@MacBook-Pro-de-MacBook-2.local>
2018-10-08 19:03:59 -0300

    Aula 06

---
+++ CADS2018/Exercícios/Exerc_06.R
@@ -0,0 +1,21 @@
+# Exercícios aula 05
+lista.de.pacotes = c("tidyverse","lubridate","janitor","readxl","stringr","repmis","janitor") # escreva a lista de pacotes
+novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
+                                      installed.packages()[,"Package"])]
+if(length(novos.pacotes) > 0) {install.packages(novos.pacotes)}
+lapply(lista.de.pacotes, require, character.only=T)
+rm(lista.de.pacotes,novos.pacotes)
+gc()
+
+
+
+
+# Crie um objeto contendo informações sobre os tamanhos das bancadas dos ----
+# partidos (arquivo `bancadas.rds`), suas respectivas coligações 
+# eleitorais para 2018 (arquivo `coligacoes.xlsx`) e o 
+# grau de concordância com a agenda do Gov 
+# Temer (arquivo `governismo_temer.xlsx`). 
+
+# Bônus: use `group_by` e `summarise` para identificar qual candidato tem a ----
+# coligação com menor média de concordância e qual candidato 
+# tem a maior proporção total de assentos.
